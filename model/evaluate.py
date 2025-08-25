72b33104bd9f1bd810014809a626aa56b6b8c23e
sohanprajapati126-create <sohanprajapati126@gmail.com>
2025-08-25 11:12:42 +0530

    Initial Commit 1.3

--- model/evaluate.py
+++ model/evaluate.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 #Load Dataset
-data = pd.read_csv('data/iris_model)
+data = pd.read_csv('data/iris_model')
 
 #Preprocess the dataset
 X = data.drop('species', axis = 1)
