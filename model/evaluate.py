06534646c13b1ac06ebacc84e7425ef54eceefdd
sohanprajapati126-create <sohanprajapati126@gmail.com>
2025-08-25 10:26:40 +0530

    Initial Commit 1.1

---
+++ model/evaluate.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+#Load Dataset
+data = pd.read_csv('data/iris_model)
+
+#Preprocess the dataset
+X = data.drop('species', axis = 1)
+y = data['species']
+
+#Split the data into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#Train RandomForest model
+model = RandomForestClassifier(n_estimators = 100, random_state=42)
+model.fit(X_train, y_train)
+
+#Save the model
+joblib.dump(model, 'model/iris_model.pkl')
+
+#Make Predictions
+y_pred = model.predict(X_test)
+
+#Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Model accuracy: {accuracy:.2f}')
