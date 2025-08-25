89bb04b7561aac5069322172c9d79fe11c885c7f
sohanprajapati126-create <sohanprajapati126@gmail.com>
2025-08-25 11:21:27 +0530

    Initial Commit 1.4

--- model/evaluate.py
+++ model/evaluate.py
@@ -13,12 +13,8 @@ y = data['species']
 #Split the data into training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#Train RandomForest model
-model = RandomForestClassifier(n_estimators = 100, random_state=42)
-model.fit(X_train, y_train)
-
-#Save the model
-joblib.dump(model, 'model/iris_model.pkl')
+#Load the saved model
+model = joblib.load('model/iris_model.pkl')
 
 #Make Predictions
 y_pred = model.predict(X_test)
