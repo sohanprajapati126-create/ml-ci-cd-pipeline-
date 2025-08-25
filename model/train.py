69749a27dfbb68be634c0dd808cf56b2f2d150db
sohanprajapati126-create <sohanprajapati126@gmail.com>
2025-08-25 11:06:26 +0530

    Initial Commit 1.2

--- model/train.py
+++ model/train.py
@@ -15,7 +15,7 @@ y = data['species']
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
 #train the Random classifier model
-model = RandomForestClassifer(n_estimators=100, random_state=42)
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 #Save the model 
